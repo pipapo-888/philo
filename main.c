a34d58f2858077ff5d10f02c02e89f088d526a7d
pipapo-888 <kazuya2002uk@gmail.com>
2025-12-02 22:07:00 +0900

    akdfjalk

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 16:45:08 by knomura           #+#    #+#             */
-/*   Updated: 2025/11/18 22:23:17 by knomura          ###   ########.fr       */
+/*   Updated: 2025/12/02 22:06:18 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,16 +52,26 @@ void insert_value(t_rule *rule, int ac, char **argv)
 		rule->number_of_times_each_philo_eat = ft_atoi(argv[5]);
 }
 
+void *routine(void *arg)
+{
+	t_philo *p = (t_philo *)arg;
+	printf("this is a routine ");
+	printf("%d %ld\n", p->id, p->thread);
+}
+
 void make_thread(t_rule *rule)
 {
 	t_philo philos[rule->number_of_philo];
 	int i = 0;
 	while (i < rule->number_of_philo)
 	{
-		pthread_create(philos[i].thread, )
-
+		philos[i].id = i;
+		pthread_create(&philos[i].thread, NULL, routine, &philos[i]);
 		i++;
 	}
+
+	for (i = 0; i < rule->number_of_philo; i++)
+		pthread_join(philos[i].thread, NULL);
 }
 
 int main(int argc, char **argv)
@@ -74,9 +84,10 @@ int main(int argc, char **argv)
 	insert_value(&rule, argc - 1, argv);
 	make_thread(&rule);
 
-	printf("%d %d %d %d", rule.number_of_philo, rule.time_to_die,
-		   rule.time_to_eat, rule.time_to_sleep);
-	if (argc == 6)
-		printf(" %d", rule.number_of_times_each_philo_eat);
+	// printf("%d %d %d %d", rule.number_of_philo, rule.time_to_die,
+	// 	   rule.time_to_eat, rule.time_to_sleep);
+	// if (argc == 6)
+	// 	printf(" %d", rule.number_of_times_each_philo_eat);
+
 	return (0);
 }
