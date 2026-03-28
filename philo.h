6b1c5a59f3cfcf2a4ec9292d2d67d87a78eb8428
Kazuya <kazuya2002uk@gmail.com>
2026-03-28 18:54:39 +0900

    ヘッダー

--- philo.h
+++ philo.h
@@ -1,5 +1,38 @@
 #ifndef PHILO_H
 #define PHILO_H
 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+typedef struct s_rule
+{
+	int				number_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philo_eat;
+	long			start_time;
+}					t_rule;
+
+typedef struct s_philo
+{
+	int				id;
+	pthread_t		thread;
+	long			last_eat_time;
+	int				meal_count;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	void			*data;
+}					t_philo;
+
+typedef struct s_data
+{
+	t_rule			rule_data;
+	t_philo			*philos_data;
+	pthread_t		thread;
+}					t_data;
 
 #endif
