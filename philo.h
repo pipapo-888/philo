a2ca70a951076060e74eafaeb508ff2f194c63eb
Kazuya <kazuya2002uk@gmail.com>
2026-03-29 19:03:46 +0900

    last_eat_timeを更新をmutexで管理

--- philo.h
+++ philo.h
@@ -22,6 +22,7 @@ typedef struct s_philo
 	int				id;
 	pthread_t		thread;
 	long			last_eat_time;
+	pthread_mutex_t	eat_time;
 	int				meal_count;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
