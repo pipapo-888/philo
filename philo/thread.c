3d8168c618d4c3add6fdca8ea152da699e17d1b0
Kazuya <kazuya2002uk@gmail.com>
2026-04-09 17:16:06 +0900

    eattimemutex削除

--- philo/thread.c
+++ philo/thread.c
@@ -6,22 +6,31 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 16:45:08 by knomura           #+#    #+#             */
-/*   Updated: 2026/04/09 16:33:15 by knomura          ###   ########.fr       */
+/*   Updated: 2026/04/09 17:12:06 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	destroy_forks(t_data *data, int count, int status)
+static int	destroy_forks(t_data *data, int fork_count, int eat_count,
+		int status)
 {
 	int	i;
 
 	i = 0;
-	while (i < count)
+	while (i < fork_count)
 	{
 		pthread_mutex_destroy(&data->forks[i]);
 		i++;
 	}
+	i = 0;
+	while (i < eat_count)
+	{
+		pthread_mutex_destroy(&data->philos_data[i].eat_time);
+		i++;
+	}
+	pthread_mutex_destroy(&data->death_flag_mtx);
+	pthread_mutex_destroy(&data->all_ate_mtx);
 	free(data->forks);
 	free(data->philos_data);
 	return (status);
@@ -52,14 +61,19 @@ int	make_thread(t_data *data)
 	data->forks = malloc(sizeof(pthread_mutex_t)
 			* data->rule_data.number_of_philo);
 	if (!data->forks)
+	{
+		pthread_mutex_destroy(&data->death_flag_mtx);
+		pthread_mutex_destroy(&data->all_ate_mtx);
+		free(data->philos_data);
 		return (1);
+	}
 	i = 0;
 	data->rule_data.start_time = t_ms();
 	while (i < data->rule_data.number_of_philo)
 	{
 		ret = pthread_mutex_init(&data->forks[i], NULL);
 		if (ret)
-			return (destroy_forks(data, i, ret));
+			return (destroy_forks(data, i, i, ret));
 		data->philos_data[i].id = i + 1;
 		data->philos_data[i].right_fork = &data->forks[i];
 		data->philos_data[i].left_fork = &data->forks[(i + 1)
@@ -68,11 +82,11 @@ int	make_thread(t_data *data)
 		data->philos_data[i].last_eat_time = data->rule_data.start_time;
 		ret = pthread_mutex_init(&data->philos_data[i].eat_time, NULL);
 		if (ret)
-			return (destroy_forks(data, i + 1, ret));
+			return (destroy_forks(data, i + 1, i, ret));
 		ret = pthread_create(&data->philos_data[i].thread, NULL, routine,
 				&data->philos_data[i]);
 		if (ret)
-			return (destroy_forks(data, i + 1, ret));
+			return (destroy_forks(data, i + 1, i + 1, ret));
 		i++;
 	}
 	pthread_create(&data->thread, NULL, check_death, data);
